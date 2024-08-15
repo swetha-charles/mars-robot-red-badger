cf348fb0387863fa6f0a3b6388888f3ef8b9fc4b
Swetha Charles <swethacharles10@gmail.com>
2024-08-15 17:29:59 +0100

    Add call to main to run robots with test data

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log('Hello world')
+import { runRobots } from "./runRobots";
+
+const testData =
+  "5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL";
+console.log(runRobots(testData));
